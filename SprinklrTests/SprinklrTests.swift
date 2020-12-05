aa97747f47cbeb2437cbb94e18c74e2c603e544e
Jayant Kumar Yadav <kumar2396jayant@gmail.com>
2020-12-05 11:10:44 +0530

    Initial Commit

---
+++ SprinklrTests/SprinklrTests.swift
@@ -0,0 +1,34 @@
+//
+//  SprinklrTests.swift
+//  SprinklrTests
+//
+//  Created by B0206635 on 05/12/20.
+//  Copyright © 2020 B0206635. All rights reserved.
+//
+
+import XCTest
+@testable import Sprinklr
+
+class SprinklrTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
